3caccf3547857ec3d0c0ac80dda3949838008682
2312303 <2312303@PC38>
2025-10-31 10:06:28 +0500

    Add project files.

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Models
+{
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}

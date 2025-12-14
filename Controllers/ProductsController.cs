6870a13fa9605f6ee20314c0c4b41d415eb3c68d
Aneeq Hassan <aneeqh0345@gmail.com>
2025-12-14 20:25:42 +0500

    product

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -46,6 +47,7 @@ namespace Technova_ecom.Controllers
         }
 
         // GET: Products/Create
+        [Authorize(Roles = "Vemdor ,Admin")]
         public IActionResult Create()
         {
             ViewBag.Categories = new SelectList(_context.Categories, "CategoryId", "CategoryName");
